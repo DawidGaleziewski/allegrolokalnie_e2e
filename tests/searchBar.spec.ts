f9f6e136b7273733b01c274318f12d91e358144a
Rafalb <rafalmichalblaszczuk@gmail.com>
2023-12-23 23:25:59 +0100

    Poprawki w teście search bar

--- tests/searchBar.spec.ts
+++ tests/searchBar.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('placeholder in input still says "czego szukasz"', async ({ page }) => {
-    // 0 run the tests with npx playrigtht test
+test('placeholder in input still says "czego szukasz?"', async ({ page }) => {
+    // Przejdź do strony Allegro Lokalnie
     await page.goto('https://allegrolokalnie.pl');
-    // 1 use locator to find the search input
-    const searchBar = undefined;
-    // 2 get the placeholder value of that element as a string
-    // 3 put it in this variable
-    const textString = undefined;
-    expect(textString).toBe("czego szukasz?")
-})
+
+    // Znajdź search bar za pomocą selektora id
+    const searchBar = await page.$('#suggests-search');
+
+    // Sprawdź, czy search bar został znaleziony
+    expect(searchBar).toBeTruthy();
+
+    // Pobierz wartość atrybutu placeholder
+    const placeholderValue = await searchBar.getAttribute('placeholder');
+
+    // Sprawdź, czy placeholder ma oczekiwaną wartość
+    expect(placeholderValue).toBe('czego szukasz?');
+});
