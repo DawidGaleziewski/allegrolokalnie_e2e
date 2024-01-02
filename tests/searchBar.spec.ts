8d013e236b1458496b18c5e178e76f3fbb56d032
Rafalb <rafalmichalblaszczuk@gmail.com>
2024-01-02 14:14:11 +0100

    adding something

--- tests/searchBar.spec.ts
+++ tests/searchBar.spec.ts
@@ -15,4 +15,6 @@ test('placeholder in input still says "czego szukasz?"', async ({ page }) => {
 
     // Sprawdź, czy placeholder ma oczekiwaną wartość
     expect(placeholderValue).toBe('czego szukasz?');
+
+    //wpisać hasło do wyszukiwania i znaleźć coś
 });
