abc54eee92619cd56da1f4094abbc6b55114d057
dawid <dawid.galeziewski@gmail.com>
2024-01-02 23:19:23 +0100

    update: add test for checking if search works

--- tests/searchText.spec.ts
+++ tests/searchText.spec.ts
@@ -5,31 +5,23 @@ test.only('able to go to search for ps5 items', async ({ page }) => {
     await page.goto('https://allegrolokalnie.pl');
 
     //Klikniecie pop up - zgody
-
-    const popupPromise = page.waitForSelector('[data-testid="accept_home_view_action"]');
-    const popup = await popupPromise;
+    const popupPromise = await page.waitForSelector('[data-testid="accept_home_view_action"]' ,{ timeout: 1000 });
     //Znajdz przycisk ok zgód
-    const acceptButton = await page.$('[data-testid="accept_home_view_action"]');
-    
+    const acceptButton = await page.getByTestId('accept_home_view_action');
     await acceptButton?.click();
 
     // Znajdź search bar za pomocą selektora id
     const searchBar = await page.$('#suggests-search');
-
-    // Sprawdź, czy search bar został znaleziony
-    expect(searchBar).toBeTruthy();
-
     //wpisanie tekstu do search bar
     await searchBar?.fill('Ps5');
 
     // Znajdź przycisk wyszukaj za pomocą pełnego selektora
-    const searchButton = await page.$('[data-testid="header-search-submit__button"]');
-
-    // Sprawdź, czy przycisk wyszukaj został znaleziony
-    expect(searchButton)?.toBeTruthy();
+    const searchButton = await page.getByTestId('header-search-submit__button');
 
     // Kliknij przycisk wyszukaj !!!! psuje test
     await searchButton?.click();
-    await page.keyboard.press('Enter');
-   // await searchButton.click();
+
+    // find header text
+
+    expect(page.getByTestId('mlc-listing-title')).toHaveText('Szukasz Ps5')
 });
